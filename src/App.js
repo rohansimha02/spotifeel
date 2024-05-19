cc1ebfadef58bbffc65030067ec1d5572c2b3e59
Davidxuwuhu <bingsen@uw.edu>
2024-05-19 09:06:51 -0700

    add all pages

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
-console.log('hello');
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Search from './components/Search';
+import Profile from './components/Profile';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={Search} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
