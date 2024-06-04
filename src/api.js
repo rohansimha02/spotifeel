0ef1ea3e0b1c837efcd4d8370ac972c4b8820183
rohansimha <rohans24@uw.edu>
2024-06-03 20:48:52 -0700

    Updates

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const getSongs = async () => {
-    const response = await fetch('http://localhost:5000/api/songs');
-    return response.json();
-};
-
-export const getRecommendations = async (emotion) => {
-    const response = await fetch(`http://localhost:5000/api/recommend?emotion=${emotion}`);
-    return response.json();
-};
